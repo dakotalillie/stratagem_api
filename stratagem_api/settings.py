48d94310b24f480aaefcd2998f4efd7724e22228
Dakota Lillie <dakota.lillie@icloud.com>
2018-04-08 16:27:56 -0400

    set up postgresql

--- stratagem_api/settings.py
+++ stratagem_api/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+from . import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -77,8 +78,12 @@ WSGI_APPLICATION = 'stratagem_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config.DATABASE_CONFIG['name'],
+        'USER': config.DATABASE_CONFIG['user'],
+        'PASSWORD': config.DATABASE_CONFIG['password'],
+        'HOST': config.DATABASE_CONFIG['host'],
+        'PORT': config.DATABASE_CONFIG['port']
     }
 }
 
