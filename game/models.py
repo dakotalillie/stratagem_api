eea23b3477809ddea8f67a4db6e6225f84b409d3
Dakota Lillie <dakota.lillie@icloud.com>
2018-04-08 02:11:59 -0400

    start working on models, remove db from git

--- game/models.py
+++ game/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Game(models.Model):
+    title = models.CharField(max_length=50)
+
+
+class Country(models.Model):
+    COUNTRIES = (
+        ('Austria', 'Austria'),
+        ('England', 'England'),
+        ('France', 'France'),
+        ('Germany', 'Germany'),
+        ('Italy', 'Italy'),
+        ('Russia', 'Russia'),
+        ('Turkey', 'Turkey'),
+    )
+    name = models.CharField(max_length=7, choices=COUNTRIES)
--- stratagem_api/settings.py
+++ stratagem_api/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'game.apps.GameConfig'
+    'game'
 ]
 
 MIDDLEWARE = [
