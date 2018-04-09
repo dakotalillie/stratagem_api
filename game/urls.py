2c9d0a967636a4abc93b5a8e08deb9179d358a2a
Dakota Lillie <dakota.lillie@icloud.com>
2018-04-08 20:37:44 -0400

    working on auth

--- game/urls.py
+++ game/urls.py
@@ -1,8 +1,10 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.authtoken import views as rest_framework_views
 
 from . import views
 
 urlpatterns = [
+    path('login/', rest_framework_views.obtain_auth_token),
     path('games/', views.GamesList.as_view()),
     path('games/<uuid:pk>/', views.GamesDetail.as_view()),
     path('games/<uuid:pk>/orders', views.OrdersList.as_view())
--- stratagem_api/settings.py
+++ stratagem_api/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
     'game'
 ]
 
@@ -125,3 +126,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated'
+    )
+}
